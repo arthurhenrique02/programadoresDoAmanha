d94f77d20a0694e3d0cb558ab4169b910b12ca08
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2024-01-16 17:41:17 -0300

    telinha simples feito para mostrar como utilizar condicionais no js

---
+++ javaScript/aula02/js/script.js
@@ -0,0 +1,11 @@
+// cria variavel pra armazernar idade
+let idade = 18;
+
+
+// checa se é maior de idade ou não
+if (idade >= 18) {
+    console.log("Maior de idade. Pode tirar a carteira de habilitação!");
+}
+else {
+    console.log("Menor de idade. Não pode tirar a carteira de habilitação!");
+}
