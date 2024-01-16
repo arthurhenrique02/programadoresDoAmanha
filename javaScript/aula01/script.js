01546b4f3f18c11f146cfde3f5c1a00fec84afbd
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2024-01-16 17:40:53 -0300

    tela simples para mostrar funcionalidades iniciais do js

---
+++ javaScript/aula01/script.js
@@ -0,0 +1,7 @@
+let notaJoao = "9"; // string
+let notaRyan = 10; // number (int)
+
+// ao somar ele 'concatena' (juntar) as strings ao inves de somar os nums
+console.log(notaJoao + notaRyan); // mostra: 910
+
+console.log(Number.parseInt(notaJoao) + notaRyan); // mostra: 19
